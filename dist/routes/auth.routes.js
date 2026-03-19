2bea47ff0b29247d4153d5d757950940408d8342
Prachiaher9 <prachiaher86052@gmail.com>
2026-03-19 16:36:38 +0530

    chore: remove dist from tracking

--- dist/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controllers_1 = require("../controllers/auth.controllers");
-const router = (0, express_1.Router)();
-router.post("/register", auth_controllers_1.userRegisterController);
-exports.default = router;
